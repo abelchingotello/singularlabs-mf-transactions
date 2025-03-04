211904692c1d283307032927178b990ede501df2
Deyner Ballena <debasan48@gmail.com>
2025-03-04 09:29:06 -0500

    enviormmt

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  URL_API_GATEWAY:'https://2l0psf3uv9.execute-api.us-east-1.amazonaws.com/stage'
+  URL_API_GATEWAY:'https://uc47epbobe.execute-api.us-east-1.amazonaws.com/stage'
 };
