7f9863a8c6ac5599d7d2a1511619eb6fe3bd25db
paulVera2001 <pveramolla@gmail.com>
2025-12-18 18:19:05 -0500

    mejora en mf-transactions para buscar solo personas activas

--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -9,15 +9,17 @@ import { environment } from 'src/environments/environment';
 export class PersonService {
 
   private readonly url = `${environment.URL_API_GATEWAY}`;
+  //private readonly url = `${environment.URL_API_LOCAL}`;
 
   constructor(
     private readonly httpClient: HttpClient,
   ) { }
 
-  getPerson(typeEntity?: string, nameAlias?: string): Observable<any> {
+  getPerson(typeEntity?: string, nameAlias?: string, activeOnly?: boolean): Observable<any> {
     let params = new HttpParams();
     if (typeEntity) { params = params.set('typeEntity', typeEntity); };
     if (nameAlias) { params = params.set('nameAlias', nameAlias); };
+    if (activeOnly) { params = params.set('activeOnly', activeOnly); };
 
     return this.httpClient.get(`${this.url}/person/entity`, { params: params });
   }
