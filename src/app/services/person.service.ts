18be8753115083afcf6dc83bc7ae48b80b1b4ea8
paulVera2001 <pveramolla@gmail.com>
2026-01-21 16:26:21 -0500

    mejora en mf-transactions para obtener solo personas activas en la bandeja de asignacion de saldo

--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -22,11 +22,13 @@ export class PersonService {
     return this.httpClient.post(`${this.url}/person/${id}`, null);
   }
 
-  getPerson(typeEntity?: string, nameAlias?: string): Observable<any> {
+  getPerson(typeEntity?: string, nameAlias?: string, activeOnly?: boolean): Observable<any> {
     let params = new HttpParams();
     if (typeEntity) params = params.set('typeEntity', typeEntity);
 
-    if (nameAlias) params = params.set('nameAlias', nameAlias);
+    if (nameAlias) params = params.set('nameAlias', nameAlias);  
+    
+    if (activeOnly) params = params.set('activeOnly', JSON.stringify(activeOnly));
 
     return this.httpClient.get(`${this.url}/person/entity`, { params: params });
   }
