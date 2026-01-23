0a292623c7024d21b293eaec59b0652193639c77
paulVera2001 <pveramolla@gmail.com>
2026-01-23 00:32:08 -0500

    homologacion en mf-transactions sobre listado de personas con tipado duro

--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -18,7 +18,7 @@ export class PersonService {
     let params = new HttpParams();
     if (typeEntity) { params = params.set('typeEntity', typeEntity); };
     if (nameAlias) { params = params.set('nameAlias', nameAlias); };
-    if (activeOnly) { params = params.set('activeOnly', activeOnly); };
+    if (activeOnly) { params = params.set('activeOnly', JSON.stringify(activeOnly)); };
 
     return this.httpClient.get(`${this.url}/person/entity`, { params: params });
   }
