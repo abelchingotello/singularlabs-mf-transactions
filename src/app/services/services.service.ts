fcf4902430daf55e3f3609d30f9c0073e5e5003a
paulVera2001 <pveramolla@gmail.com>
2026-01-24 03:14:20 -0500

    homologado en mf-transactions para traer personas activas en el flujo de asignar saldo

--- src/app/services/services.service.ts
+++ src/app/services/services.service.ts
@@ -47,15 +47,6 @@ export class ServicesService {
     return this.httpClient.post<any>(`${this.url}/services/${id}`,null);
   }
 
-  getPerson(typeEntity?:string,nameAlias?:string):Observable<any> {
-    let params = new HttpParams();
-    if(typeEntity) params = params.set('typeEntity', typeEntity);
-    
-    if(nameAlias) params = params.set('nameAlias', nameAlias);
-
-    return this.httpClient.get(`${this.url}/person/entity`, {params:params});
-  }
-
   updateService(data:any):Observable<any>{
     return this.httpClient.patch(`${this.url}/services/status`,data);
   }
